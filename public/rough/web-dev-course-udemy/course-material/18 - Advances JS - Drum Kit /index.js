10be7b7c162a4e9f09fa44931e77274442183950
Dharmendra Rathod <rathoddharmendra.business@gmail.com>
2024-10-15 16:45:53 +0200

    20241015.v1645 - added audio to drum kit

--- public/rough/web-dev-course-udemy/course-material/18 - Advances JS - Drum Kit /index.js
+++ public/rough/web-dev-course-udemy/course-material/18 - Advances JS - Drum Kit /index.js
@@ -1,11 +1,40 @@
 
 document.querySelector('button').addEventListener('click', handleClick);
 
-function handleClick(){
-    console.log('I got clicked!');
-    new Audio('./sounds/crash.mp3').play();
+function handleClick() {
+    console.log();
+
+    // this.classList.add('pressed');
+
+    switch (this.innerHTML) {
+        case 'w':
+            new Audio('./sounds/tom-1.mp3').play();
+            break;
+        case 'a':
+            new Audio('./sounds/tom-2.mp3').play();
+            break;
+        case 's':
+            new Audio('./sounds/tom-3.mp3').play();
+            break;
+        case 'd':
+            new Audio('./sounds/tom-4.mp3').play();
+            break;
+        case 'j':
+            new Audio('./sounds/crash.mp3').play();
+            break;
+        case 'k':
+            new Audio('./sounds/kick-bass.mp3').play();
+            break;
+        case 'l':
+            new Audio('./sounds/snare.mp3').play();
+            break;
+
+        default:
+            console.log(`wrong key pressed ${this.innerHTML}`);
+            break;
+    }
 }
 
-for (let i = 0; i < document.querySelectorAll('button').length; i++){
-    document.querySelectorAll('button')[i].addEventListener('click', handleClick);
+for (let i = 0; i < document.querySelectorAll('button').length; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', handleClick);
 }
